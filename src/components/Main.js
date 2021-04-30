8237f4ff877aeb41055b51b88a701baf174bb53c
Nikolay Rademacher <47133234+nrademacher@users.noreply.github.com>
2021-04-30 07:15:09 +0200

    Delete Main.js

--- src/components/Main.js
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-const Main = ({ children }) => (React.createElement("section", { className: "px-4 md:px-0 grid place-content-center md:container md:mx-auto" }, children));
-export default Main;
