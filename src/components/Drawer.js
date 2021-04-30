866202263fd79a70e3f927b0af9151c7cf191d64
Nikolay Rademacher <47133234+nrademacher@users.noreply.github.com>
2021-04-30 07:14:16 +0200

    Delete Drawer.js

--- src/components/Drawer.js
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-const Drawer = ({ show, children }) => {
-    return (React.createElement("section", { className: `p-8 fixed top-0 w-full h-96 md:h-80 z-20 bg-white shadow transform ${show ? 'translate-y-0' : '-translate-y-96 md:-translate-y-80'} transition-transform duration-500 ease` }, children));
-};
-export default Drawer;
