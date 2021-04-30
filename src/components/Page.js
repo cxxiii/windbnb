3e344ba4798094b8a9f2d57d32b6ce3d0b122a97
Nikolay Rademacher <47133234+nrademacher@users.noreply.github.com>
2021-04-30 07:15:23 +0200

    Delete Page.js

--- src/components/Page.js
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-const Page = ({ children }) => (React.createElement("article", { className: "mb-16 text-gray-900 text-shadow-sm subpixel-antialiased" }, children));
-export default Page;
