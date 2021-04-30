4a2b5261165b71096a4e1851f642c976b1411f88
Nikolay Rademacher <47133234+nrademacher@users.noreply.github.com>
2021-04-30 07:14:27 +0200

    Delete DrawerBackdrop.js

--- src/components/DrawerBackdrop.js
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-const DrawerBackdrop = ({ closeDrawer }) => (React.createElement("div", { onClick: closeDrawer, className: "cursor-pointer fixed w-full h-full bg-[rgba(0,0,0,0.5)] z-10 top-0 right-0" }));
-export default DrawerBackdrop;
